cef98bad5b5125216a27d316663e3b369efc12cd
Satya Prakash <swadeshicreator@gmail.com>
2026-06-25 13:25:58 +0530

    Add custom backend url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import dotenv from "dotenv";
 
+dotenv.config();
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.BACKEND_URL,
   withCredentials: true,
 });
 
